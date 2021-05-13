506f71f4f8e1109c8d53162af4951bff220a303f
McLutzifer <lukasschumi@gmx.at>
2021-05-13 15:12:00 +0200

    add sum function

--- SoloLearn/argsAndFunc.go
+++ SoloLearn/argsAndFunc.go
@@ -10,8 +10,19 @@ func convert(x int) {
 	fmt.Println(x / 100)
 }
 
+func mult(x, y int) {
+	fmt.Println(x * y)
+}
+
+func sum(x, y int) int {
+	return x + y
+}
+
 func main() {
 	welcome("David")
 	welcome("Lukas")
 	convert(300)
+	mult(3, 4)
+	result := sum(42, 8)
+	fmt.Println(result)
 }
