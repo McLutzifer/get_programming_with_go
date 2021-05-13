ba9b9badd64bdfd0a68a544b7da930007c8c414b
McLutzifer <lukasschumi@gmx.at>
2021-05-13 15:08:33 +0200

    add convert function

--- SoloLearn/argsAndFunc.go
+++ SoloLearn/argsAndFunc.go
@@ -6,7 +6,12 @@ func welcome(name string) {
 	fmt.Println("Hello, " + name)
 }
 
+func convert(x int) {
+	fmt.Println(x / 100)
+}
+
 func main() {
 	welcome("David")
 	welcome("Lukas")
+	convert(300)
 }
